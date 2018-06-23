e51717dcfa87e9539efdb8413d05308a534cbb91
PIYAWAT PITAKPAWATKUL (Lazada Group) <jo459_36@hotmail.com>
2018-06-23 22:59:59 +0700

    setup Prod

---
+++ README.md
@@ -0,0 +1,4 @@
+Seller 360
+----------------
+Run: yarn install to install all the dependencies.
+Run: yarn run mock-server to start local mock server
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CompressionPlugin = require('compression-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  console.log('env',env);
+  const CSSExtract = new ExtractTextPlugin('styles.css');
+ 
+  return {
+  //entry: './src/playground/redux-expensify.js',
+  //entry: './src/playground/destructuring.js',
+    plugins: [
+    new BundleAnalyzerPlugin(),
+    new CompressionPlugin({
+    algorithm: 'gzip'
+    }),
+    CSSExtract
+  ],
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }, {
+      test: /\.s?css$/,
+     use: CSSExtract.extract({
+     use: [
+       {
+        loader: 'css-loader',
+         options: {
+           sourceMap: true
+         }
+       },
+       {
+         loader: 'sass-loader',
+         options: {
+           sourceMap: true
+         }
+       }
+      ]
+     })
+    },{
+      test: /\.less$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'less-loader'
+      ]
+    }
+	]
+  },
+  devtool: isProduction ? 'source-map' :'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true
+  }
+};
+};
