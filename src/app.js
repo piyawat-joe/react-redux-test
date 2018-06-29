90d81438b222a03c79f1ddf87bddbf35f952707d
PIYAWAT PITAKPAWATKUL (Lazada Group) <jo459_36@hotmail.com>
2018-06-29 23:51:02 +0700

    Expensify style

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ import './styles/styles.scss';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
 const jsx = (
@@ -26,11 +27,10 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
-    console.log('uid',user.uid);
     store.dispatch(login(user.uid));
     store.dispatch(startSetExpenses()).then(() => {
       renderApp();
